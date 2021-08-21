84a91b73a81a5163f926718e4fd158213d20aa63
Victor Rosa <victorsouzarosa100@gmail.com>
2021-08-21 19:11:52 -0300

    initial commit

---
+++ game.js
@@ -0,0 +1,70 @@
+let game = {
+
+        techs : [
+        'bootstrap',
+        'css',
+        'python',
+        'firebase',
+        'html',
+        'javascript',
+        'github',
+        'nodejs',
+        'golang',
+        'react',],
+
+    cards: null,
+
+
+    createCardsFromTechs: function() {
+
+        this.cards = [];
+
+        this.techs.forEach((tech)=> {
+            this.cards.push(this.createPairFromTech(tech));
+        })
+    
+        this.cards = this.cards.flatMap(pair => pair);
+        this.shuffleCards();
+
+        
+        return this.cards;
+
+    },
+    
+    createPairFromTech: function(tech) {
+        return [
+            {
+                id: this.createIdWithTech(tech),
+                icon: tech,
+                flipped: false,
+    
+            },   
+
+            {
+                id: this.createIdWithTech(tech),
+                icon: tech,
+                flipped: false,
+            }
+    ]
+    },
+    
+    createIdWithTech: function(tech) {
+    
+        return tech + parseInt(Math.random() * 1000);
+    
+    },
+
+    shuffleCards: function(cards) {
+        let currentIndex = this.cards.length;
+        let randomIndex = 0;
+    
+        while (currentIndex != 0) {
+    
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+    
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
+        }
+    
+    },
+}
---
+++ script.js
@@ -0,0 +1,61 @@
+const FRONT = "card_front"
+const BACK = "card_back"
+const ICON= "icon"
+const CARD = "card"
+
+
+startGame();
+
+function startGame() {
+    initializeCards(game.createCardsFromTechs());
+}
+    
+function initializeCards(cards) {
+        let gameBoard = document.getElementById("gameBoard");
+        
+        game.cards.forEach(card => {
+
+            let cardElement =  document.createElement('div');
+            cardElement.id = card.id;
+            cardElement.classList.add(CARD);
+            cardElement.dataset.icon = card.icon;
+
+            createCardContent(card, cardElement);
+
+            cardElement.addEventListener('click', flipCard)
+            gameBoard.appendChild(cardElement);
+        })
+}
+
+
+function createCardContent(card, cardElement){
+    
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+}
+
+function createCardFace(face, card, element){
+
+    let cardElementFace =  document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+
+    element.appendChild(cardElementFace);
+
+}
+
+
+
+
+function flipCard() {
+    this.classList.add("flip");
+}
